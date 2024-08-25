0d1d1e0f453dc26a7c592e612638e0de1ad11600
GilmarSilva <gilmarsilvadf@gmail.com>
2024-08-24 21:58:27 -0300

    Questao 37 feita

--- exercicios65algortimos/questao35.py
+++ exercicios65algortimos/questao35.py
@@ -1,1 +1,17 @@
 # Arquivo questao35.py
+
+
+# 35. Desenvolva um algoritmo que peça ao usuário para digitar dois números e verifique se a multiplicação deles é igual a 20.
+
+# Solicita ao usuário para digitar dois números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Calcula a multiplicação dos números
+resultado = numero1 * numero2
+
+# Verifica se o resultado é igual a 20
+if resultado == 20:
+    print("A multiplicação é igual a 20!")
+else:
+    print("A multiplicação não é igual a 20.")
--- exercicios65algortimos/questao36.py
+++ exercicios65algortimos/questao36.py
@@ -1,1 +1,20 @@
 # Arquivo questao36.py
+
+
+# 36. Crie um programa que solicite ao usuário um número de 1 a 12 e exiba o mês correspondente.
+
+# Solicita ao usuário para digitar um número de 1 a 12
+numero_mes = int(input("Digite um número inteiro entre 1 e 12: "))
+
+# Verifica se o número está dentro do intervalo válido
+if 1 <= numero_mes <= 12:
+    # Lista com os nomes dos meses
+    meses = ["Janeiro", "Fevereiro", "Março", "Abril","Maio", "Junho", "Julho", "Agosto","Setembro", "Outubro", "Novembro", "Dezembro"]
+    # Exibe o mês correspondente
+    print(f"O mês correspondente ao número {numero_mes} é {meses[numero_mes-1]}.")
+else:
+    print("Número inválido. Digite um valor entre 1 e 12.")
+
+
+# {numero_mes} é substituído pelo valor da variável numero_mes.
+# {meses[numero_mes - 1]} é substituído pelo nome do mês correspondente ao número inserido pelo usuário.
