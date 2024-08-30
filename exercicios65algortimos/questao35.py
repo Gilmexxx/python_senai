b07e08074548c6f79025f89bd73166674fb8be22
Gilmar Silva <gilmarsilvadf@gmail.com>
2024-08-30 17:50:50 -0300

    Fazendo jogo velha por modulo

--- exercicios65algortimos/questao35.py
+++ exercicios65algortimos/questao35.py
@@ -1,6 +1,5 @@
 # Arquivo questao35.py
 
-<<<<<<< HEAD
 # 35. Desenvolva um algoritmo que peça ao usuário para digitar dois números e verifique se a multiplicação deles é igual a 20.
 
 numero = int(input('Digite o primeiro numero'))
@@ -12,20 +11,3 @@ if multiplicao == 20:
     print('A multiplicacao dos dois numero é IGUAL A 20')
 else:
     print("A multiplicacao dos dosi numeros NAO É IGUAL 20")
-=======
-
-# 35. Desenvolva um algoritmo que peça ao usuário para digitar dois números e verifique se a multiplicação deles é igual a 20.
-
-# Solicita ao usuário para digitar dois números
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
-
-# Calcula a multiplicação dos números
-resultado = numero1 * numero2
-
-# Verifica se o resultado é igual a 20
-if resultado == 20:
-    print("A multiplicação é igual a 20!")
-else:
-    print("A multiplicação não é igual a 20.")
->>>>>>> 0d1d1e0f453dc26a7c592e612638e0de1ad11600
